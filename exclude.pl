2ef942655bf416c836336ad7c777ba22119e501e
brian d foy <brian.d.foy@gmail.com>
2021-03-13 22:15:04 -0500

    Output some progress info

--- exclude.pl
+++ exclude.pl
@@ -3,35 +3,76 @@
 # it drops paths that match the listed exclusions
 # output is null delimited to match input
 use File::Basename;
+use File::Spec::Functions;
 
 my $dirname = dirname(__FILE__);
-my $exclude_file = $dirname.'/excludes.txt';
-my $only_file = $dirname.'/only.txt';
 
-sub file_to_re {
+*DEBUG = *STDOUT;
+
+sub items_from_file {
   my ($file, $fallback) = @_;
   my @items;
   if (-e $file) {
     open FILE, '<', $file;
     local $/=undef;
     my $file=<FILE>;
-    for (split /\R/, $file) {
-      next if /^#/;
+    for (split /\R+/, $file) {
+      next if /^\s*#/;
       s/^\s*(.*)\s*$/$1/;
       push @items, $_;
     }
   }
+}
+
+sub items_to_re {
+  my @items = @_;
   my $pattern = scalar @items ? join "|", @items : $fallback;
   return $pattern;
 }
 
-my $exclude = file_to_re($exclude_file, '^$');
-my $only = file_to_re($only_file, '.');
+say DEBUG "Filtering file paths";
 
-$/="\0";
+my $exclude_file = catfile( $dirname, 'excludes.txt' );
+my $exclude_pattern = do {
+	if( -e $exclude_file ) {
+	  print DEBUG "Found <$exclude_file>\n";
+	  my @items = items_from_file( $exclude_file );
+	  items_to_re( @items, '^$');
+	}
+	else {
+	  print DEBUG "No file <$exclude_file>, so skipping that\n";
+	  qr/^$/;
+	}
+};
+
+my $only_file = catfile( $dirname, 'only.txt' );
+my $only_pattern = do {
+	if( -e $exclude_file ) {
+	  print DEBUG "Found <$only_file>\n";
+	  my @items = items_from_file( $only_file );
+	  items_to_re( @items, '.');
+	}
+	else {
+	  print DEBUG "No file <$only_file>, so skipping that\n";
+	  qr/./;
+	}
+};
+
+my $only    = items_to_re( $only_file,     '.');
+
 while (<>) {
   chomp;
-  next if m{$exclude};
-  next unless m{$only};
-  print "$_$/";
+  if( m{$exclude_pattern} ) {
+    say DEBUG "File <$_> excluded by exclude pattern";
+    next;
+  }
+  elsif( ! m{$only_pattern} ) {
+    say DEBUG "File <$_> excluded by exclude pattern";
+    next;
+  }
+  else {
+    say DEBUG "File <$_> included";
+  }
+
+  print "$_\0";
 }
